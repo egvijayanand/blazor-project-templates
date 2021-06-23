aec15a8fde657c7d6e0a1bbdd3a52e095cf9497f
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2021-06-23 11:44:54 +0530

    Project template updated to .NET 6 Preview 5

--- src/BlazorWinForms/Install-Template.bat
+++ src/BlazorWinForms/Install-Template.bat
@@ -1,6 +1,6 @@
 :: Installs the NuGet package
 @echo off
 echo Installing the project template . . .
-dotnet new --install .\bin\Debug\VijayAnand.WindowsForms.Blazor.1.0.0.nupkg
+dotnet new --install .\bin\Debug\VijayAnand.WindowsForms.Blazor.1.0.1.nupkg
 echo Process completed
 pause
