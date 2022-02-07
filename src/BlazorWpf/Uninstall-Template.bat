6a2ccd5c7a9cae503d88ee8e88646518020bfee6
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-02-07 11:10:01 +0530

    Individual templates updated to .NET MAUI Preview 12

--- src/BlazorWpf/Uninstall-Template.bat
+++ src/BlazorWpf/Uninstall-Template.bat
@@ -1,6 +1,19 @@
 :: Removes the installed project template
 @echo off
-echo Uninstalling the project template . . .
-dotnet new --uninstall VijayAnand.Wpf.Blazor
-echo Process completed
+
+:: Package Name
+
+if not exist PackageName.txt (call Error "Package name file not available." & goto end)
+
+set /P packageName=<PackageName.txt
+
+if [%packageName%]==[] (call Error "Package name not configured." & goto end)
+
+call Info "Uninstalling the %packageName% template ..."
+
+dotnet new --uninstall %packageName%
+
+if %errorlevel% == 0 (call Success "Process completed.") else (call Error "Package uninstall failed.")
+
+:end
 pause
