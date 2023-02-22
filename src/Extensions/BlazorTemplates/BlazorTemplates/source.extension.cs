44928b9509ea6ffb6687e747cd043df5addb33b8
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2023-02-23 02:33:37 +0530

    VS Extension updated to support .NET 8 Preview 1

--- src/Extensions/BlazorTemplates/BlazorTemplates/source.extension.cs
+++ src/Extensions/BlazorTemplates/BlazorTemplates/source.extension.cs
@@ -11,8 +11,8 @@ namespace VijayAnand.BlazorTemplates
         public const string Name = "Blazor Desktop Templates";
         public const string Description = @"Project template for hosting the Blazor component in the Windows Forms and WPF desktop application.";
         public const string Language = "en-US";
-        public const string Version = "2.1.1.0";
+        public const string Version = "3.0.0.0";
         public const string Author = "Vijay Anand E G";
-        public const string Tags = "Blazor, Desktop, WindowsForms, WinForms, WPF, Windows, Template, BlazorWebView, Hybrid, Razor, Web, GA, .NET MAUI, Client, Modernization, .NET 7";
+        public const string Tags = "Blazor, Desktop, WindowsForms, WinForms, WPF, Windows, Template, BlazorWebView, Hybrid, Razor, Web, GA, .NET MAUI, Client, Modernization, .NET 7, .NET 8";
     }
 }
