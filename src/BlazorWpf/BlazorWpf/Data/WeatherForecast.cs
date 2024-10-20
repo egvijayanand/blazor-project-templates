b5566b4c730d16f795b070964c4b4118a4afb2e1
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2024-10-20 11:28:14 +0530

    Templates updated for .NET MAUI 9 RC2 #104

--- src/BlazorWpf/BlazorWpf/Data/WeatherForecast.cs
+++ src/BlazorWpf/BlazorWpf/Data/WeatherForecast.cs
@@ -6,12 +6,12 @@ namespace BlazorWpf._1.Data
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
+        public DateTime Date { get; init; }
 
-        public int TemperatureC { get; set; }
+        public int TemperatureC { get; init; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string? Summary { get; init; }
     }
 }
